a1da4398376ef01806b9aa8b7dc89b3ae18bf9c2
Jimena Rodriguez <jimenaangelica.rodriguez.next@bbva.com>
2022-10-13 19:12:56 -0500

    sqs aws spring

---
+++ payment-service/payment-services/src/main/java/mx/com/example/services/facade/impl/PaymentFacade.java
@@ -0,0 +1,35 @@
+package mx.com.example.services.facade.impl;
+
+import mx.com.example.commons.to.PaymentEventTO;
+import mx.com.example.commons.to.TicketEventTO;
+import mx.com.example.commons.to.UserTO;
+import mx.com.example.services.facade.IPaymentFacade;
+import mx.com.example.services.service.IPaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class PaymentFacade implements IPaymentFacade {
+
+    @Autowired
+    private IPaymentService paymentService;
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public List<UserTO> getAllUsers() {
+        return this.paymentService.getUsers();
+    }
+
+    @Override
+    public PaymentEventTO authorizeCard(TicketEventTO ticketEvent) {
+
+        PaymentEventTO paymentEvent = new PaymentEventTO();
+        
+
+        kafkaTemplate.send("payment_events", paymentEvent);
+        return paymentEvent;
+    }
+}
