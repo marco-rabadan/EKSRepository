a3aacf18bfcb9b5fcf6d8348dcad874ad5945229
Jimena Rodriguez <jimenaangelica.rodriguez.next@bbva.com>
2022-10-18 16:12:40 -0500

    create sqs

--- infra/aws-sqs.tf
+++ infra/aws-sqs.tf
@@ -1,14 +1,45 @@
-resource "aws_sqs_queue" "terraform_queue" {
+resource "aws_sqs_queue" "ticket_events" {
   name                        = "ticket_events.fifo"
   fifo_queue                  = true
+  content_based_deduplication = true
+}
+resource "aws_sqs_queue" "order_events" {
+  name                        = "order_events.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+}
+resource "aws_sqs_queue" "payment_events" {
+  name                        = "payment_events.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
 }
 
-output "sqsurl" {
-  value = aws_sqs_queue.terraform_queue.url
+output "ticket_events_url" {
+  value = aws_sqs_queue.ticket_events.url
 }
-output "sqsarn" {
-  value = aws_sqs_queue.terraform_queue.arn
+output "ticket_events_arn" {
+  value = aws_sqs_queue.ticket_events.arn
 }
-output "sqsid" {
-  value = aws_sqs_queue.terraform_queue.id
+output "ticket_events_id" {
+  value = aws_sqs_queue.ticket_events.id
+}
+
+output "order_events_url" {
+  value = aws_sqs_queue.ticket_events.url
+}
+output "order_events_arn" {
+  value = aws_sqs_queue.ticket_events.arn
+}
+output "order_events_id" {
+  value = aws_sqs_queue.ticket_events.id
+}
+
+output "payment_events_url" {
+  value = aws_sqs_queue.ticket_events.url
+}
+output "payment_events_arn" {
+  value = aws_sqs_queue.ticket_events.arn
+}
+output "payment_events_id" {
+  value = aws_sqs_queue.ticket_events.id
 }
